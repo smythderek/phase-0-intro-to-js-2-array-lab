e450b57b110a7bc84ed40f3a4643ef5adca9c1a2
Derek Smyth <smyth.derek@gmail.com>
2023-02-21 08:40:24 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+function prependCat(name) {
+    const newerArray = [name, ...cats];
+    return newerArray;
+}
+
+function removeLastCat() {
+    const evenNewerArray = cats.slice(0,2); 
+    return evenNewerArray;
+}
+
+function removeFirstCat() {
+    const theNewNewArray = cats.slice(-2);
+    return theNewNewArray;
+}
